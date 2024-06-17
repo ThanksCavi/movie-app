21817b22dea2a32806c5fde6635196174794e412
Alina Evgeneva <ThanksCavi@gmail.com>
2024-06-17 04:44:42 +0300

    Task

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -15,25 +15,3 @@ import { MatIconModule } from '@angular/material/icon';
     bootstrap: [AppComponent],
 })
 export class AppModule { }
-
-
-// import { RouterModule, Routes } from '@angular/router';
-// import { MovieListComponent } from './components/movie-list/movie-list.component';
-// import { MovieCardComponent } from './components/movie-card/movie-card.component';
-
-// const routes: Routes = [
-//     { path: '', component: MovieListComponent }
-// ];
-
-// @NgModule({
-//     declarations: [
-//         AppComponent,
-//         MovieListComponent,
-//         MovieCardComponent
-//     imports: [
-//         RouterModule.forRoot(routes)
-//     ],
-//     providers: [],
-//     bootstrap: [AppComponent]
-// })
-// export class AppModule { }
